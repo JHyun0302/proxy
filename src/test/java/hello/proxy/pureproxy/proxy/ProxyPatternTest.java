49e9bcd9937b86ff1bbbd736652f7c952d42a6d1
jaehyun <wogustkfkd11@gmail.com>
2023-02-08 23:17:44 +0900

    디자인: 프록시 패턴 - 접근제어

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,35 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+    /**
+     * 런타임 객체 의존 관계
+     * client -> realSubject
+     */
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    /**
+     * 런타임 객체 의존 관계
+     * client -> proxy -> realSubject
+     */
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
