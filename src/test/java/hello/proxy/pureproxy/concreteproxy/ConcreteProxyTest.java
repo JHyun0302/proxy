b4eabe5f9ef7a572a61e238ad24a1c231c91aabf
jaehyun <wogustkfkd11@gmail.com>
2023-02-09 12:09:12 +0900

    인터페이스 없을때 상속으로 프록시 기술 삽입 예시

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,30 @@
+package hello.proxy.pureproxy.concreteproxy;
+
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+    /**
+     * 프록시 도입 전: 런타임 객체 의존 관계
+     * client -> concreteLogic
+     */
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+    /**
+     * 프록시 도입: 런타임 객체 의존 관계
+     * client -> timeProxy -> concreteLogic
+     */
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+}
